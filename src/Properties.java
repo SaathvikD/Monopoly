7a7e52c00cc02dafc5e3beae68fda3cd9c0175f0
SaathvikD <dsaathvik@gmail.com>
2022-11-17 13:44:54 -0600

    Started Properties Class added link for rents

--- src/Properties.java
+++ src/Properties.java
@@ -3,6 +3,7 @@ public class Properties {
     String color;
     int price,housePrice,baseRent,rentOne,rentTwo,rentThree,rentFour,rentHotel;
     Public Properties(){
+        https://www.falstad.com/monopoly.html
 
     }
 }
