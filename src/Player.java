30da061e349b24c818279f7b23e92d5d9647acf6
jpasc <Jpascualsr06@gmail.com>
2022-11-28 13:20:11 -0600

    player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,10 @@
 public class Player {
+    // Color, placement, properties, railroads, money, utilities
+
+    private int placement = 0, money = 1500, utilities = 0, railroads = 0;
+    private String color;
+    //how to initiate properties?
+
+
+
 }
